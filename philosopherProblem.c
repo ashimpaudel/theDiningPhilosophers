2c1bd6c8a8569df9e8a95f9c602b90ae2aa53520
ashimpaudel <ashimpaudel@gmail.com>
2018-03-03 05:11:34 -0500

    working for n philosophers

--- philosopherProblem.c
+++ philosopherProblem.c
@@ -4,42 +4,49 @@
 
 typedef struct{
 	int id;
+	int totalPhilo;
 }pArgs;
 
 void return_forks(int philosopher_number);
 
-void pickup_forks(int philosopher_number);
+void pickup_forks(int philosopher_number, int totalPhilo);
 pthread_cond_t condVar;
 pthread_mutex_t mutex;
 void *solvePhilosophy(void* args);
-int forks[5];
+int forks[1000];
 
-int main(void){
-
+int main(int argc, char*argv[]){
+	if (argc != 2){
+		printf("Format to run the program is:$ program [numberOfPhilosopher]");
+		exit(0);
+	}
+	int totalPhilo;
+	totalPhilo  = atoi(argv[1]);
 	pthread_mutex_init (&mutex, NULL);
 	pthread_cond_init (&condVar,NULL);
 	
 	//creating five philosopher, representing each of them with integer
 	int i;
 	srand(time(NULL)); 
-	int philosophers[] = {0,1,2,3,4};
+	//int philosophers[] = {0,1,2,3,4};
 	
-	for (i=0; i<5; i++)
+	for (i=0; i<totalPhilo; i++)
 		forks[i]=0;
 	
 
 
 	//create five threads
-	pthread_t threads[5];
+	pthread_t threads[totalPhilo];
 	
 	//call the threads
 	while(1)
 	{
 
-		for(i=0; i<5; i++){
+		for(i=0; i<totalPhilo; i++){
 			//instantiate the argument to be passed
 			pArgs argm;
 			argm.id = i;
+			argm.totalPhilo = totalPhilo;
 			pthread_t th;
 			threads[i] = th;
 			pthread_create(&threads[i], NULL,solvePhilosophy , &argm);
@@ -56,14 +63,14 @@ void *solvePhilosophy(void* args){
 	//as soon as thinking state is done, philosopher is hungry and wish to grab chopsticks, i.e. enter the critical section
         
 	printf("philosopher %d is now trying to pick up forks\n",((pArgs*)args)->id);
-	pickup_forks(((pArgs*)args)->id);	
+	pickup_forks(((pArgs*)args)->id,((pArgs*)args)->totalPhilo);	
 
 
 }
 
-void pickup_forks(int philosopher_number){
+void pickup_forks(int philosopher_number, int totalPhilo){
 	int right = philosopher_number;
-	int left = (philosopher_number + 1)%5;
+	int left = (philosopher_number + 1)%totalPhilo;
 	int locked;
 		
 	//Before philosopher can enter critical section, checking is need to be performed
