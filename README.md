cad26365174672a44bb1a20b8c4572e5445dc0ec
ashimpaudel <ashimpaudel@gmail.com>
2018-03-08 21:53:53 -0500

    solves the bouneded waiting problem

--- README.md
+++ README.md
@@ -11,4 +11,5 @@ This program is the solution to the dining-philosopher's problem using p_threads
 # Snapshot of a running progarm
 ![alt text](https://image.ibb.co/g6yqDS/Screenshot_from_2018_03_05_23_42_10.png)
 
-
+# Note to instructor
+The program on master branch does not make use of pthread_cond_t variable. There is a separate program that uses condition variable on the branch called singleMutex. The version on singleMutex branch makes use of all the APIs and variable types stated on the text book but does not solve the bounded waiting problem. To run singleMutex branch the instructions are same.
--- philosopherProblem.c
+++ philosopherProblem.c
@@ -1,98 +1,86 @@
 #include <stdio.h>
-#include <pthread.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <pthread.h>
+
+void             return_forks(int philosopher_number, int totalPhilo);
+void             pickup_forks(int philosopher_number, int totalPhilo);
+void*            solvePhilosophy(void* args);
+pthread_mutex_t* mutex;
 
 typedef struct{
-	int id;
-	int totalPhilo;
+	        int id;
+	        int totalPhilo;
 }pArgs;
 
-void return_forks(int philosopher_number);
 
-void pickup_forks(int philosopher_number, int totalPhilo);
-pthread_cond_t condVar;
-pthread_mutex_t mutex;
-void *solvePhilosophy(void* args);
-int* forks;
 
-int main(int argc, char*argv[]){
-	if (argc != 2){
-		printf("Format to run the program is:$ program [numberOfPhilosopher]");
-		exit(0);
+int main(int argc, char** argv){
+	if (argc != 2) {
+		printf("Invalid Format. Format to run program is ./solvePhilosophy [n]");
+		exit(1);
 	}
-	int totalPhilo;
-	totalPhilo  = atoi(argv[1]);
-	pthread_mutex_init (&mutex, NULL);
-	pthread_cond_init (&condVar,NULL);
 	
-	//creating five philosopher, representing each of them with integer
-	int i;
-	srand(time(NULL)); 
-	//int philosophers[] = {0,1,2,3,4};
-	forks = malloc(totalPhilo*sizeof(int));
-	for (i=0; i<totalPhilo; i++)
-		forks[i]=1;
 	
+	int   totalPhilo;
+	int   i;
 
-
-	//create five threads
-	pthread_t threads[totalPhilo];
+	totalPhilo = atoi(argv[1]);
+	mutex      = (pthread_mutex_t*)malloc(sizeof(pthread_mutex_t)*totalPhilo); //mutex = forks
 	
-	//call the threads
-	while(1)
+	for (i = 0; i < totalPhilo; ++i)
+		pthread_mutex_init(&mutex[i],NULL);
+	
+	srand(time(NULL));
+
+	pthread_t* threads;
+        threads	= (pthread_t*)malloc(totalPhilo*sizeof(pthread_t));                 //threads = philosophers
+	for (i = 0; i < totalPhilo; ++i)
 	{
 
-		for(i=0; i<totalPhilo; i++){
-			//instantiate the argument to be passed
-			pArgs argm;
-			argm.id = i;
-			argm.totalPhilo = totalPhilo;
-			pthread_t th;
-			threads[i] = th;
-			pthread_create(&threads[i], NULL,solvePhilosophy , &argm);
-		}
+	        pArgs argm;
+		argm.id = i;
+		argm.totalPhilo = totalPhilo;
+		pthread_create(&threads[i],NULL,solvePhilosophy,&argm);
 	}
 
+	for (i = 0; i < totalPhilo; ++i)
+		pthread_join(threads[i],NULL);
 
+	return 0;
 }
 
-void *solvePhilosophy(void* args){
-	//simulating the thinking state
-	printf("philosopher %d is now thinkinking                                              #\n",((pArgs*)args)->id);
-	sleep(rand()%3);
-	//as soon as thinking state is done, philosopher is hungry and wish to grab chopsticks, i.e. enter the critical section
-        
-	printf("philosopher %d is now trying to pick up forks                                  #\n",((pArgs*)args)->id);
-	pickup_forks(((pArgs*)args)->id,((pArgs*)args)->totalPhilo);	
-
-
+void* solvePhilosophy(void* args){
+	 
+	int phil_id = ((pArgs*)args)->id;
+	int totalPhilo = ((pArgs*)args)->totalPhilo;
+	
+	while (1) {
+		printf("philosopher %d is now thinking                                             #\n",phil_id);
+		sleep(rand()%3); 
+		printf("philosopher %d is trying to pickup fork                                    #\n",phil_id);
+		pickup_forks(phil_id, totalPhilo);			
+		sleep(rand()%3); 
+		return_forks(phil_id, totalPhilo);
+	}
+	return NULL;
 }
 
 void pickup_forks(int philosopher_number, int totalPhilo){
-	int right = philosopher_number;
-	int left = (philosopher_number + 1)%totalPhilo;
-	int locked;
-		
-	//Before philosopher can enter critical section, checking is need to be performed
-	pthread_mutex_lock(&mutex);
-	while(!forks[left] || !forks[right]) 
-		pthread_cond_wait(&condVar, &mutex);
-	//Entering C.S
-	//Simulate the behavior of eating by pausing thread the process/thread
-	printf("philosopher %d is now eating ###################################################\n", philosopher_number);
-	sleep(rand()%3);
-	return_forks(philosopher_number);
-			
-	
+	int left = philosopher_number;
+	int right= (philosopher_number+1)%totalPhilo;
+	pthread_mutex_lock(&mutex[left]); 
+	pthread_mutex_lock(&mutex[right]); 
+	printf("philosopher %d is now eating################################################\n", philosopher_number);
+}
+
+void return_forks(int philosopher_number, int totalPhilo){
+	int left = philosopher_number;
+	int right= (philosopher_number+1)%totalPhilo;
+	printf("philosopher %d is done eating###############################################\n", philosopher_number);
+	pthread_mutex_unlock(&mutex[left]);  
+	pthread_mutex_unlock(&mutex[right]); 
+
 }
 
 
-
-void return_forks(int philosopher_number){
-	printf("philosopher %d done eating #####################################################\n\n",philosopher_number);
-	pthread_cond_signal(&condVar);
-	pthread_mutex_unlock(&mutex);
-	//philosopher returns back to thinking state
-	sleep(rand()%3);
-}
